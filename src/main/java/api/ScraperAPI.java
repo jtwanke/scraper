286dca77163056fe45ca837aa767934e90f49805
jtwanke <jtwanke@comast.net>
2017-03-18 22:59:56 -0700

    Increasing readability.

--- src/main/java/api/ScraperAPI.java
+++ src/main/java/api/ScraperAPI.java
@@ -33,95 +33,35 @@ public class ScraperAPI {
 	private final static int YELP_MODE = 0;
 	private final static int YP_MODE = 1;
 	
-	private final static String YELP = "yelp";
+	private final static String YELP = "Yelp";
 	private final static String YELP_ID = "yelp";
-	private final static String YP = "yellowpages";
+	private final static String YP = "YellowPages";
 	private final static String YP_ID = "yellowpages";
 	
 	// build generic function that handles logic
-
-//	@GET
-//	@Path("/yellowpages")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public Response ypSearch(@QueryParam("phrase") String phrase, @QueryParam("loc") String loc) {
-//		System.out.println("Reached 'search/yellowpages/" + phrase);
-//		String output = "{\"status\":\"FAILURE\", \"err\":\"Invalid phrase : " + phrase + "\", \"errnum\" : \""
-//				+ ERR_PHRASE + "\"}";
-//		if (phrase.length() > 0) {
-//			try {
-//				YellowPagesFetcher fetcher = new YellowPagesFetcher(URLEncoder.encode(phrase, "UTF-8"), URLEncoder.encode(loc, "UTF-8"));
-//				Document doc = fetcher.Fetch();
-//				if(doc != null){
-//					YellowPagesParser parser = new YellowPagesParser(doc);
-//					Link[] theLinks = parser.Parse();
-//					Gson objGson = new GsonBuilder().setPrettyPrinting().create();
-//					String mapToJson = objGson.toJson(theLinks);
-//					output = "{\"status\":\"SUCCESS\", \"tableid\"" + " \"yp\"," + " \"domain\"" + " \"Yellow Pages\"," + "\"input\": \"" + phrase + "\", \"result\": " + mapToJson + "}";
-//					return Response.status(200).entity(output).build();
-//				}
-//			} catch (UnsupportedEncodingException e) {}
-//		}
-//		return Response.status(200).entity(output).build();
-//	}
-	
-//	@GET
-//	@Path("/yellowpages")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public Response ypSearch(@QueryParam("phrase") String phrase, @QueryParam("loc") String loc) {
-//		System.out.println("Reached 'search/yellowpages/" + phrase);
-//		String output = "{\"status\":\"FAILURE\", \"err\":\"Invalid phrase : " + phrase + "\", \"errnum\" : \""
-//				+ ScraperAPI.ERR_PHRASE + "\"}";
-//		if (phrase.length() > 0 && loc.length() > 0) { //trivial validation
-//			try {
-//				Link[] theLinks = this.search(phrase, loc, ScraperAPI.YP_MODE); //this is new method call
-//				if(theLinks != null){
-//					String mapToJson = ScraperAPI.JsonMapLinks(theLinks);
-//					output = "{\"status\":\"SUCCESS\", \"tableid\"" + " \"yelp\"," + " \"domain\"" + " \"Yelp\"," + "\"input\": \"" + phrase + "\", \"result\": " + mapToJson + "}";
-//					return Response.status(200).entity(output).build();
-//				}
-//			} catch (UnsupportedEncodingException e) {
-//				output = ScraperAPI.errorString(phrase, loc);
-//			}
-//		}
-//		return Response.status(200).entity(output).build();
-//	}
-	
-//	@GET
-//	@Path("/yelp")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public Response yelpSearch(@QueryParam("phrase") String phrase, @QueryParam("loc") String loc) {
-//		System.out.println("Reached 'search/yelp/" + phrase);
-//		String output = "{\"status\":\"FAILURE\", \"err\":\"Invalid phrase : " + phrase + "\", \"errnum\" : \""
-//				+ ScraperAPI.ERR_PHRASE + "\"}";
-//		if (phrase.length() > 0 && loc.length() > 0) { //trivial validation
-//			try {
-//				Link[] theLinks = this.search(phrase, loc, ScraperAPI.YELP_MODE); //this is new method call
-//				if(theLinks != null){
-//					String mapToJson = ScraperAPI.JsonMapLinks(theLinks);
-//					output = "{\"status\":\"SUCCESS\", \"tableid\"" + " \"yelp\"," + " \"domain\"" + " \"Yelp\"," + "\"input\": \"" + phrase + "\", \"result\": " + mapToJson + "}";
-//					return Response.status(200).entity(output).build();
-//				}
-//			} catch (UnsupportedEncodingException e) {
-//				output = ScraperAPI.errorString(phrase, loc);
-//			}
-//		}
-//		return Response.status(200).entity(output).build();
-//	}
 	@GET
 	@Path("/yellowpages")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response ypSearch(@QueryParam("phrase") String phrase, @QueryParam("loc") String loc) {
-		return Response.status(200).entity(this.start(phrase, loc, ScraperAPI.YP_ID, ScraperAPI.YP_MODE)).build();
+		return Response.status(200).entity(this.start(phrase, loc, ScraperAPI.YP_ID, ScraperAPI.YP, ScraperAPI.YP_MODE)).build();
 	}
 	
 	@GET
 	@Path("/yelp")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response yelpSearch(@QueryParam("phrase") String phrase, @QueryParam("loc") String loc) {
-		return Response.status(200).entity(this.start(phrase, loc, ScraperAPI.YELP_ID, ScraperAPI.YELP_MODE)).build();
+		return Response.status(200).entity(this.start(phrase, loc, ScraperAPI.YELP_ID, ScraperAPI.YELP, ScraperAPI.YELP_MODE)).build();
 	}
 	
-	private String start(String phrase, String loc, String tabid, int mode){
+	/**
+	 * Starts the execution of a site scraping. Does generic message-building and exception-catching.
+	 * @param phrase The phrase you are searching for on the target site.
+	 * @param loc The location the results should be tailored to.
+	 * @param tabid The table ID of the results.
+	 * @param mode The target site.
+	 * @return The message to be built into the HTTP response.
+	 */
+	private String start(String phrase, String loc, String tabid, String domain, int mode){
 		if (phrase.length() > 0 && loc.length() > 0) { //trivial validation
 			try {
 				Link[] theLinks = this.search(phrase, loc, mode); //this is new method call
@@ -129,7 +69,7 @@ public class ScraperAPI {
 					String mapToJson = ScraperAPI.JsonMapLinks(theLinks);
 					return "{\"status\":\"SUCCESS\"," +
 						   "\"tableid\":" + " \"" + tabid + "\"," + 
-					       "\"domain\":" + " \"" + tabid +"\"," + 
+					       "\"domain\":" + " \"" + domain +"\"," + 
 						   "\"input\":"  + " \"" + phrase + "\"," + 
 					       "\"result\":" + mapToJson + "}";
 				}
@@ -139,6 +79,14 @@ public class ScraperAPI {
 		return ScraperAPI.errorString(phrase, loc);
 	}
 	
+	/**
+	 * Performs the search of the target site. Initializes core classes to capture and parse HTML content from target site.
+	 * @param phrase The phrase you are searching for on the target site.
+	 * @param loc The location the results should be tailored to.
+	 * @param mode The target site.
+	 * @return The links returned from the search of the target site.
+	 * @throws UnsupportedEncodingException
+	 */
 	private Link[] search(String phrase, String loc, int mode) throws UnsupportedEncodingException{
 		HtmlDocFetcher fetcher;
 		Parser parser;
@@ -156,6 +104,13 @@ public class ScraperAPI {
 		
 	}
 	
+	/**
+	 * Creates a HtmlDocFetcher object corresponding to the arguments. 
+	 * @param phrase The phrase you are searching for on the target site.
+	 * @param loc The location the results should be tailored to.
+	 * @param mode The target site.
+	 * @return a HtmlDocFetcher object corresponding to the arguments.
+	 */
 	private HtmlDocFetcher createFetcher(String phrase, String loc, int mode){
 		switch(mode){
 		case ScraperAPI.YELP_MODE:
@@ -167,6 +122,12 @@ public class ScraperAPI {
 		}
 	}
 	
+	/**
+	 * Creates a Parser object corresponding to the arguments.
+	 * @param doc The JSoup Document object to be parsed.
+	 * @param mode The target (source) website.
+	 * @return a Parser object corresponding to the arguments.
+	 */
 	private Parser createParser(Document doc, int mode){
 		switch(mode){
 		case ScraperAPI.YELP_MODE:
